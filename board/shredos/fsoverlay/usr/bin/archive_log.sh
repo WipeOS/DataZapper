5a5ab3d6efbd493bedc5c5cb1b6b761b879f12de
Tad Dolphay <tdolphay@wipeos.com>
2023-10-25 09:23:14 -0500

    Added nwipe options
    Added psplash launch options to show splash screen

--- board/shredos/fsoverlay/usr/bin/archive_log.sh
+++ board/shredos/fsoverlay/usr/bin/archive_log.sh
@@ -27,15 +27,17 @@ fi
 # Create the temporary directory we will mount the FAT32 partition onto.
 if [ ! -d "$archive_drive_directory" ]; then
     mkdir "$archive_drive_directory"
+
     if [ $? != 0 ]; then
-                printf "archive_log.sh: Unable to create the temporary mount directory $archive_drive_directory\n"
-                exit_code=2
+		printf "archive_log.sh: Unable to create the temporary mount directory $archive_drive_directory\n"
+		exit_code=2
     fi
 fi
 
 # mount the FAT32 partition onto the temporary directory
 mount $drive $archive_drive_directory
 status=$?
+
 if [ $status != 0 ] && [ $status != 32 ]; then
     # exit only if error, except code 32 which means already mounted
     printf "archive_log.sh: Unable to mount the FAT32 partition $drive to $archive_drive_directory\n"
@@ -46,14 +48,16 @@ else
     # Copy the dmesg.txt file over to the FAT32 partition
     dmesg > dmesg.txt
     cp /dmesg.txt "$archive_drive_directory/"
+
     if [ $? != 0 ]; then
-	printf "archive_log.sh: Unable to copy the dmesg.txt file to the root of $drive:/\n"
+		printf "archive_log.sh: Unable to copy the dmesg.txt file to the root of $drive:/\n"
     else
-	printf "archive_log.sh: Sucessfully copied dmesg.txt to $drive:/\n" 
+		printf "archive_log.sh: Sucessfully copied dmesg.txt to $drive:/\n"
     fi
 
     # Copy the nwipe log files over to the FAT32 partition
     cp /nwipe_log* "$archive_drive_directory/"
+
     if [ $? != 0 ]; then
         printf "archive_log.sh: Unable to copy the nwipe log files to the root of $drive:/\n"
     else
@@ -62,20 +66,22 @@ else
         # Create the temporary sent directory we will move log files that have already been copied
         if [ ! -d "$sent_directory" ]; then
             mkdir "$sent_directory"
+
             if [ $? != 0 ]; then
-                        printf "archive_log.sh: Unable to create the temporary directory $sent_directory on the RAM disc\n"
-                        exit_code=5
+				printf "archive_log.sh: Unable to create the temporary directory $sent_directory on the RAM disc\n"
+				exit_code=5
             fi
         fi
 
         if [ exit_code != 5 ]; then
                 # Move the nwipe logs into the RAM disc sent directory
                 mv /nwipe_log* "$sent_directory/"
+
                 if [ $? != 0 ]; then
-                            printf "archive_log.sh: Unable to move the nwipe logs into the $sent_directory on the RAM disc\n"
-                            exit_code=6
+					printf "archive_log.sh: Unable to move the nwipe logs into the $sent_directory on the RAM disc\n"
+					exit_code=6
                 else
-                            printf "archive_log.sh: Moved the nwipe logs into the $sent_directory\n"
+					printf "archive_log.sh: Moved the nwipe logs into the $sent_directory\n"
                 fi
         fi
     fi
@@ -84,9 +90,10 @@ fi
 # unmount the FAT32 drive
 sleep 1
 umount "$archive_drive_directory"
+
 if [ $? != 0 ]; then
-                printf "archive_log.sh: Unable to unmount the FAT32 partition\n"
-                exit_code=7
+	printf "archive_log.sh: Unable to unmount the FAT32 partition\n"
+	exit_code=7
 else
     printf "archive_log.sh: Successfully unmounted $archive_drive_directory ($drive)\n"
 fi
@@ -94,4 +101,5 @@ fi
 if [ $exit_code != 0 ]; then
     printf "archive_log.sh: Failed to copy nwipe log files to $drive, exit code $exit_code\n"
 fi
+
 exit $exit_code
