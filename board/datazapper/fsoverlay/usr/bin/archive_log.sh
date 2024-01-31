7f831eb9d0393602de5d1d0e61e865a8f8685ff4
Tad Dolphay <tdolphay@wipeos.com>
2024-01-31 15:28:37 -0600

    Changed nwipe log to datazapper log

--- board/datazapper/fsoverlay/usr/bin/archive_log.sh
+++ board/datazapper/fsoverlay/usr/bin/archive_log.sh
@@ -18,10 +18,10 @@ sent_directory="/sent"
 drive=$(fdisk -l | grep -i '/dev/*' | grep -i FAT32 | awk '{print $1}' | head -n 1)
 
 if [ "$drive" == "" ]; then
-	printf "archive_log.sh: No FAT32 formatted drive found, unable to archive nwipe log file\n"
+	printf "archive_log.sh: No FAT32 formatted drive found, unable to archive datazapper log file\n"
 	exit 1
 else
-	printf "Archiving nwipe logs to $drive\n"
+	printf "Archiving datazapper logs to $drive\n"
 fi
 
 # Create the temporary directory we will mount the FAT32 partition onto.
@@ -55,13 +55,14 @@ else
 		printf "archive_log.sh: Sucessfully copied dmesg.txt to $drive:/\n"
     fi
 
-    # Copy the nwipe log files over to the FAT32 partition
-    cp /nwipe_log* "$archive_drive_directory/"
+    # Copy the datazapper log files over to the FAT32 partition
+    sed -i '$ d' /datazapper_log*
+    cp /datazapper_log* "$archive_drive_directory/"
 
     if [ $? != 0 ]; then
-        printf "archive_log.sh: Unable to copy the nwipe log files to the root of $drive:/\n"
+        printf "archive_log.sh: Unable to copy the datazapper log files to the root of $drive:/\n"
     else
-        printf "archive_log.sh: Successfully copied the nwipe logs to $drive:/\n"
+        printf "archive_log.sh: Successfully copied the datazapper logs to $drive:/\n"
 
         # Create the temporary sent directory we will move log files that have already been copied
         if [ ! -d "$sent_directory" ]; then
@@ -74,14 +75,14 @@ else
         fi
 
         if [ exit_code != 5 ]; then
-                # Move the nwipe logs into the RAM disc sent directory
-                mv /nwipe_log* "$sent_directory/"
+                # Move the datazapper logs into the RAM disc sent directory
+                mv /datazapper_log* "$sent_directory/"
 
                 if [ $? != 0 ]; then
-					printf "archive_log.sh: Unable to move the nwipe logs into the $sent_directory on the RAM disc\n"
+					printf "archive_log.sh: Unable to move the datazapper logs into the $sent_directory on the RAM disc\n"
 					exit_code=6
                 else
-					printf "archive_log.sh: Moved the nwipe logs into the $sent_directory\n"
+					printf "archive_log.sh: Moved the datazapper logs into the $sent_directory\n"
                 fi
         fi
     fi
@@ -99,7 +100,7 @@ else
 fi
 
 if [ $exit_code != 0 ]; then
-    printf "archive_log.sh: Failed to copy nwipe log files to $drive, exit code $exit_code\n"
+    printf "archive_log.sh: Failed to copy datazapper log files to $drive, exit code $exit_code\n"
 fi
 
 exit $exit_code
